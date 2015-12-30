db2fc8c0304d621ce52438b1934fa6d720c297b6
Minh-Duc <nmduc@apcs.vn>
2015-12-30 21:56:17 +0700

    Change download by wget fix error certificate ssl

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y curl
-curl -sSL https://get.docker.com/ | sh
+# curl -sSL https://get.docker.com/ | sh
+wget -O - https://get.docker.com/ | bash
 sudo service docker start
 sudo groupadd docker
 sudo gpasswd -a zeus docker
